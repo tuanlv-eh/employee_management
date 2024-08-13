e1dd4c8d0404b358ff4b8199eacd4d9f61282d32
Stark <tuan.le2@employmenthero.com>
2024-08-13 13:57:20 +0700

    Update git ignore and some sample test cases

--- test/models/employee_test.rb
+++ test/models/employee_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class EmployeeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "employee is valid" do
+    employee = Employee.new
+    assert employee.valid?
+  end
+
+  test "employee has a name" do
+    employee = Employee.new(name: "John Doe")
+    assert_equal "John Doe", employee.name
+  end
+
+  test "employee has a department" do
+    employee = Employee.new(department: "Engineering")
+    assert_equal "Engineering", employee.department
+  end
+
+  test "employee has a valid salary" do
+    employee = Employee.new(salary: 5000)
+    assert_equal 5000, employee.salary
+  end
 end
